69f9c7f5d9cd82794f2528ea4920e3602b723d2f
Vigo-Tea <zc16655000128@yeah.net>
2024-11-04 21:31:11 +0800

    test create account

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"yun.tea/block/bright/account/pkg/client/account"
 	"yun.tea/block/bright/common/utils"
@@ -10,11 +11,15 @@ import (
 )
 
 func main() {
-	resp, err := account.CreateAccount(context.Background(), &proto.CreateAccountRequest{
-		Remark: "ssss",
-	})
-	if err != nil {
-		fmt.Println(err)
+	fmt.Println(time.Now())
+	for i := 0; i < 1000; i++ {
+		resp, err := account.CreateAccount(context.Background(), &proto.CreateAccountRequest{
+			Remark: "ssss",
+		})
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(utils.PrettyStruct(resp))
 	}
-	fmt.Println(utils.PrettyStruct(resp))
+	fmt.Println(time.Now())
 }
