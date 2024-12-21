ad8152bba5a6457e3edd6a407d5a74eafbb1c96c
root <zc16655000128@yeah.net>
2024-12-21 09:11:49 +0000

    add pprof port

--- main.go
+++ main.go
@@ -1,47 +1,23 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"sync"
-	"time"
-
-	"github.com/google/uuid"
-	"yun.tea/block/bright/common/utils"
-	"yun.tea/block/bright/endpoint/pkg/client/endpoint"
-	"yun.tea/block/bright/proto/bright/basetype"
-	proto "yun.tea/block/bright/proto/bright/endpoint"
+	"log"
+	"net/http"
 )
 
+var idx = 0
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	idx++
+	fmt.Println(idx)
+}
+
+func apiRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":20086", nil))
+}
+
 func main() {
-	fmt.Println(time.Now())
-	wg := sync.WaitGroup{}
-	for i := 0; i < 25; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for i := 0; i < 20; i++ {
-				name := uuid.NewString()
-				address := "test address"
-				state := basetype.EndpointState_EndpointDefault
-				rps := uint32(1)
-				remark := "ssss"
-				resp, err := endpoint.CreateEndpoint(context.Background(), &proto.CreateEndpointRequest{
-					Info: &proto.EndpointReq{
-						Name:    &name,
-						Address: &address,
-						RPS:     &rps,
-						State:   &state,
-						Remark:  &remark,
-					},
-				})
-				if err != nil {
-					fmt.Println(err)
-				}
-				fmt.Println(utils.PrettyStruct(resp))
-			}
-		}()
-	}
-	wg.Wait()
-	fmt.Println(time.Now())
+	apiRequests()
 }
